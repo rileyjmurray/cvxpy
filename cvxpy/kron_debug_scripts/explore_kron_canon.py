686f82e4c03efb5b2795b0d70eb5435f32de92a8
rileyjmurray <rjmurray@berkeley.edu>
2022-02-12 16:12:17 -0800

    move some tests and debugging scripts over from my branch to get kron working with a variable in the left argument

---
+++ cvxpy/kron_debug_scripts/explore_kron_canon.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+import cvxpy as cp
+
+"""
+kron(M, N) = [M[0,0] * N   , ..., M[0, end] * N  ]
+             [M[1,0] * N   , ..., M[1, end] * N  ]
+             ...
+             [M[end, 0] * N, ..., M[end, end] * N]
+"""
+
+
+def random_problem(z_dims, c_dims, var_left, param, seed=0):
+    np.random.seed(seed)
+    _C_value = np.random.rand(*c_dims).round(decimals=2)
+    if param:
+        _C = cp.Parameter(shape=c_dims)
+        _C.value = _C_value
+    else:
+        _C = cp.Constant(_C_value)
+    _Z = cp.Variable(shape=z_dims)
+    _L = np.random.rand(*z_dims).round(decimals=2)
+    if var_left:
+        _constraints = [cp.kron(_Z, _C) >= cp.kron(_L, _C), _Z >= 0]
+    else:
+        _constraints = [cp.kron(_C, _Z) >= cp.kron(_C, _L), _Z >= 0]
+    # ^ Only the first constraint matters. We use two constraints because that
+    # makes it easier to set conditional breakpoints in canonInterface.py.
+    #
+    #   Specifically, canonInterface.py:get_problem_matrix is called once for
+    #   the objective function and once for the constraint matrix. When it's
+    #   called for the objective function the linOps argument is a list of length
+    #   one, and when it's called for the constraint matrix the linOps argument
+    #   is a list of length equal to the number of constraints.
+    #
+    _obj_expr = cp.sum(_Z)
+
+    _prob = cp.Problem(cp.Minimize(_obj_expr), _constraints)
+    # The optimal solution is Z.value == L.
+    return _Z, _C, _L, _prob
+
+
+def run_example():
+    seed = 0
+    z_dims = (2, 2)
+    c_dims = (1, 2)
+
+    solve = False  # I haven't found much value in setting this to True
+    var_left = False  # True means we test the new functionality, with a Variable in left argument
+    param = True  # tests pass when param=False. But maybe worth looking at ...
+    Z, C, L, prob = random_problem(z_dims, c_dims, var_left, param, seed=seed)
+    if solve:
+        prob.solve(solver='ECOS')
+    else:
+        print('\nDimensions')
+        print(f'\tz_dims = {z_dims}')
+        print(f'\tc_dims = {c_dims}')
+        data = prob.get_problem_data(solver='ECOS', enforce_dpp=True)[0]
+        G = data['G'].A
+        h = data['h']
+        print(f'\nData for param={param} and var_left={var_left}')
+        print(f'\tnnz(G) = {np.count_nonzero(G)}')
+        print(f'\tnnz(h) = {np.count_nonzero(h)}\n')
+        print(G)
+        print(h)
+        print()
+
+
+if __name__ == '__main__':
+    """
+    Use this script for debugging kron.
+    It will probably help to take screenshots of debugging state variables when
+    running under the old functionality and the new functionality.
+    You can find a set of screenshots at:
+    https://docs.google.com/presentation/d/1ETKlAkz1XrSfikvnF27T-JGYF5niLoA936fOSduvy6U/edit?usp=sharing
+    feel free to edit by adding more slides.
+    As of Feb 11, the relevant slides start on page 18.
+    """
+    run_example()  # compiles incorrectly
+    run_example()  # compiles correctly
---
+++ cvxpy/tests/test_kron_canon.py
@@ -0,0 +1,113 @@
+"""
+Copyright 2022, the CVXPY developers.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+from typing import Tuple
+
+import numpy as np
+
+import cvxpy as cp
+from cvxpy.tests.base_test import BaseTest
+
+
+class TestKron(BaseTest):
+    """
+    The Kronecker product of matrices M, N is :
+        kron(M, N) = [M[0,0] * N   , ..., M[0, end] * N  ]
+                     [M[1,0] * N   , ..., M[1, end] * N  ]
+                     ...
+                     [M[end, 0] * N, ..., M[end, end] * N]
+    """
+
+    @staticmethod
+    def make_kron_prob(z_dims: Tuple[int],
+                       c_dims: Tuple[int],
+                       param: bool,
+                       var_left: bool,
+                       seed: int):
+        """
+        Construct random nonnegative matrices (C, L) of shapes
+        (c_dims, z_dims) respectively. Define an optimization
+        problem with a matrix variable of shape z_dims:
+            min sum(Z)
+            s.t.  kron(Z, C) >= kron(L, C)   ---   if var_left is True
+                  kron(C, Z) >= kron(C, L)   ---   if var_left is False
+                  Z >= 0
+        Regardless of whether var_left is True or False, the optimal
+        solution to that problem is Z = L.
+        If param is True, then C is defined as a CVXPY Parameter.
+        If param is False, then C is a CVXPY Constant.
+        A small remark: the constraint that Z >= 0 is redundant.
+        It's there because it's easier to set break points that distinguish
+        objective canonicalization and constraint canonicalization
+        when there's more than one constraint.
+        """
+        np.random.seed(seed)
+        C_value = np.random.rand(*c_dims).round(decimals=2)
+        if param:
+            C = cp.Parameter(shape=c_dims)
+            C.value = C_value
+        else:
+            C = cp.Constant(C_value)
+        Z = cp.Variable(shape=z_dims)
+        L = np.random.rand(*Z.shape).round(decimals=2)
+        if var_left:
+            constraints = [cp.kron(Z, C) >= cp.kron(L, C), Z >= 0]
+        else:
+            constraints = [cp.kron(C, Z) >= cp.kron(C, L), Z >= 0]
+        obj_expr = cp.sum(Z)
+        prob = cp.Problem(cp.Minimize(obj_expr), constraints)
+        return Z, C, L, prob
+
+
+class TestKronRightVar(TestKron):
+
+    C_DIMS = [(1, 1), (2, 1), (1, 2), (2, 2)]
+
+    """
+    If you run test_gen_kronr_param on its own, then it fails.
+    If you run test_gen_kronr_const on its own, then it passes.
+    If you run both tests at the same time, then they both pass.
+    """
+
+    def test_gen_kronr_param(self):
+        z_dims = (2, 2)
+        for c_dims in TestKronRightVar.C_DIMS:
+            Z, C, L, prob = self.make_kron_prob(z_dims, c_dims, param=True,
+                                                var_left=False, seed=0)
+            prob.solve(solver='ECOS', abstol=1e-8, reltol=1e-8)
+            self.assertEqual(prob.status, cp.OPTIMAL)
+            con_viols = prob.constraints[0].violation()
+            self.assertLessEqual(np.max(con_viols), 1e-4)
+            self.assertItemsAlmostEqual(Z.value, L, places=4)
+
+    @unittest.skip
+    def test_gen_kronr_const(self):
+        z_dims = (2, 2)
+        for c_dims in TestKronRightVar.C_DIMS:
+            Z, C, L, prob = self.make_kron_prob(z_dims, c_dims, param=False,
+                                                var_left=False, seed=0)
+            prob.solve(solver='ECOS', abstol=1e-8, reltol=1e-8)
+            self.assertEqual(prob.status, cp.OPTIMAL)
+            con_viols = prob.constraints[0].violation()
+            self.assertLessEqual(np.max(con_viols), 1e-4)
+            self.assertItemsAlmostEqual(Z.value, L, places=4)
+
+
+class TestKronRightVar_IsolateParam(TestKronRightVar):
+
+    def test_gen_kronr_param(self):
+        super().test_gen_kronr_param()
+
+    @unittest.skip
+    def test_gen_kronr_const(self):
+        super().test_gen_kronr_const()
