39cd16eb6811e087199e1a6655162675325b9f34
rileyjmurray <rjmurray@berkeley.edu>
2022-02-12 16:32:06 -0800

    more informative explore_kron_canon.py

--- kron_debug_scripts/explore_kron_canon.py
+++ kron_debug_scripts/explore_kron_canon.py
@@ -10,7 +10,7 @@ kron(M, N) = [M[0,0] * N   , ..., M[0, end] * N  ]
 """
 
 
-def random_problem(z_dims, c_dims, var_left, param, seed=0):
+def random_problem(z_dims, c_dims, param, seed=0):
     np.random.seed(seed)
     _C_value = np.random.rand(*c_dims).round(decimals=2)
     if param:
@@ -20,12 +20,10 @@ def random_problem(z_dims, c_dims, var_left, param, seed=0):
         _C = cp.Constant(_C_value)
     _Z = cp.Variable(shape=z_dims)
     _L = np.random.rand(*z_dims).round(decimals=2)
-    if var_left:
-        _constraints = [cp.kron(_Z, _C) >= cp.kron(_L, _C), _Z >= 0]
-    else:
-        _constraints = [cp.kron(_C, _Z) >= cp.kron(_C, _L), _Z >= 0]
-    # ^ Only the first constraint matters. We use two constraints because that
-    # makes it easier to set conditional breakpoints in canonInterface.py.
+    _constraints = [cp.kron(_C, _Z) >= cp.kron(_C, _L), _Z >= 0]
+    # ^ Only the first constraint matters, since _C and _L are nonnegative.
+    # We use two constraints because that makes it easier to set
+    # conditional breakpoints in canonInterface.py.
     #
     #   Specifically, canonInterface.py:get_problem_matrix is called once for
     #   the objective function and once for the constraint matrix. When it's
@@ -40,41 +38,34 @@ def random_problem(z_dims, c_dims, var_left, param, seed=0):
     return _Z, _C, _L, _prob
 
 
-def run_example():
+def run_example(solve=True):
     seed = 0
     z_dims = (2, 2)
     c_dims = (1, 2)
 
-    solve = False  # I haven't found much value in setting this to True
-    var_left = False  # True means we test the new functionality, with a Variable in left argument
     param = True  # tests pass when param=False. But maybe worth looking at ...
-    Z, C, L, prob = random_problem(z_dims, c_dims, var_left, param, seed=seed)
+    Z, C, L, prob = random_problem(z_dims, c_dims, param, seed=seed)
     if solve:
         prob.solve(solver='ECOS')
-    else:
-        print('\nDimensions')
-        print(f'\tz_dims = {z_dims}')
-        print(f'\tc_dims = {c_dims}')
-        data = prob.get_problem_data(solver='ECOS', enforce_dpp=True)[0]
-        G = data['G'].A
-        h = data['h']
-        print(f'\nData for param={param} and var_left={var_left}')
-        print(f'\tnnz(G) = {np.count_nonzero(G)}')
-        print(f'\tnnz(h) = {np.count_nonzero(h)}\n')
-        print(G)
-        print(h)
-        print()
+        print('\nVariable "Z" is the right operand in kron.\nSolving with ECOS ...')
+        violations = prob.constraints[0].violation()
+        print(f'\tReported problem status: {prob.status}')
+        print(f'\tConstraint violation: {np.max(violations)}')
+    print('\nDimensions')
+    print(f'\tz_dims = {z_dims}')
+    print(f'\tc_dims = {c_dims}')
+    data = prob.get_problem_data(solver='ECOS', enforce_dpp=False)[0]
+    G = data['G'].A
+    h = data['h']
+    print(f'\nECOS data for param={param}.')
+    print(f'\tnnz(G) = {np.count_nonzero(G)}')
+    print(f'\tnnz(h) = {np.count_nonzero(h)}\n')
+    print(G)
+    print(h)
+    print()
 
 
 if __name__ == '__main__':
-    """
-    Use this script for debugging kron.
-    It will probably help to take screenshots of debugging state variables when
-    running under the old functionality and the new functionality.
-    You can find a set of screenshots at:
-    https://docs.google.com/presentation/d/1ETKlAkz1XrSfikvnF27T-JGYF5niLoA936fOSduvy6U/edit?usp=sharing
-    feel free to edit by adding more slides.
-    As of Feb 11, the relevant slides start on page 18.
-    """
-    run_example()  # compiles incorrectly
-    run_example()  # compiles correctly
+    solve = True  # same bad results when solve=False.
+    run_example(solve)  # compiles incorrectly
+    run_example(solve)  # compiles correctly
