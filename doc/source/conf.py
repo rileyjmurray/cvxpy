fe18df6d693b377b0f0af0ab7b0cdc4ca57f6221
Riley Murray <rjmurray@berkeley.edu>
2023-01-29 18:41:17 -0800

    wordmark and favicon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -151,7 +151,7 @@ html_sidebars = {
 html_theme_options = {
    'github_user': 'cvxpy',
    'github_repo': 'cvxpy',
-   'github_banner': True,
+   #'github_banner': True,
    'github_type': 'star',
    'travis_button': False,
    'analytics_id': 'UA-50248335-1',
@@ -169,12 +169,15 @@ html_title = f'CVXPY {version} documentation'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+# html_logo = 'assets/cvxpy_parabolas_light.png'
+# html_logo = 'assets/CVXPY_Logo_CMYK_LightBG.png'
+# html_logo = 'assets/cvxpy-logo-light-small-margins.png'
+html_logo = 'assets/cvxpy-wordmark-light.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'assets/favicon_white_bg/android-chrome-192x192.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
